a8a1485bcbd759c8b7d49126df0f1dae68502187
Li-Qiushui <3159558455@qq.com>
2023-02-20 17:04:32 +0800

    “待到山花烂漫时”

---
+++ vue-chat/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+	<div id="app">
+		
+	</div>
+</template>
+
+<script>
+	import HelloWorld from './components/HelloWorld.vue'
+
+	export default {
+		name: 'App',
+		components: {
+			HelloWorld
+		}
+	}
+</script>
+
+<style>
+	#app {
+		
+	}
+</style>
---
+++ vue-chat/src/components/Login.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="login">
+
+	</div>
+</template>
+
+<script>
+	export default {
+		name:"login",
+		components:{
+			
+		},
+		mounted() {
+			
+		},
+		methods:{
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
